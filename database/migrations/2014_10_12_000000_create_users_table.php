a18cb2c4bf4cec025f712a3fad4cbb068fc72248
Karim-Muhammad <kimoomar007@gmail.com>
2024-06-26 20:51:02 +0300

    add notification table + modify users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,22 +13,35 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name'); // required
-            $table->string("family_name"); // x
-            $table->string('photo')->nullable(); // required
+            $table->string('name');
+            $table->string("family_name")->nullable();
+            $table->string('photo')->nullable();
             
-            $table->string("phone"); // required
+            $table->string("phone");
+            $table->enum("gender", ["male", "female"]);
             
-            $table->string('email')->unique(); // reuqired
+            $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
 
-            $table->string('password'); // required
+            $table->string('password');
             $table->rememberToken();
             
-            $table->string("country"); // x
-            $table->string("address"); // x
+            $table->string("country")->nullable();
+            $table->string("address")->nullable();
+
+            $table->string("blood_type")->nullable();
+            $table->string("weight")->nullable();
+            $table->string("height")->nullable();
+            $table->string("allergies")->nullable();
+            $table->string("medications")->nullable();
+
             
-            $table->date('date_of_birth'); // required
+            $table->date('date_of_birth');
+
+            $table->string('provider_id')->nullable();
+            $table->string('provider')->nullable();
+
+            $table->unique(['provider_id', 'provider']);
 
             $table->timestamps();
         });
